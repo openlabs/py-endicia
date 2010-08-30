8f50594c421ac73e939a0f35db1ab6a5dd3adce7
Sharoon Thomas <sharoon.thomas@openlabs.co.in>
2010-08-30 13:18:34 +0100

    [ADD]Additional unit tests for international
    [IMP] Copyright on the files

--- endicia/api.py
+++ endicia/api.py
@@ -1,3 +1,20 @@
+# 
+# (c) Copyright 2010: Sharoon Thomas
+# 
+# This file is part of py-endicia.
+# 
+# py-endicia is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# 
+# py-endicia is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with py-endicia.  If not, see <http://www.gnu.org/licenses/>.
+
 """
 Api for Web-based, on-demand services of the USPS
 provided by Endicia
